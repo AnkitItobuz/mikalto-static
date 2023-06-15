816e86c3683854e26956db7e220ba0e8778e83dd
IT042301 <it042301@IT042301s-MacBook-Pro.local>
2023-06-15 18:48:00 +0530

    image slider and new section added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,62 @@
-const leftScroll = document.querySelector(".left-scroll");
-const rightScroll = document.querySelector(".right-scroll");
+const leftScroll = document.querySelectorAll(".left-scroll");
+const rightScroll = document.querySelectorAll(".right-scroll");
 const slides = document.querySelectorAll(".slides");
 const standardRoom = document.querySelector(".standard-room");
 
-leftScroll.addEventListener( 'click', ()=>{
-    alert();
-slides[1].style.display="block";
-slides[0].style.display="none"
-// slides.classList.add("slides");
-// standardRoom.classList.add('slides');
+let start = 0;
+let end = slides.length - 1;
+console.log(end);
+let mid = Math.round((start + end - start) / 2);
 
+slides[mid].classList.remove("slides");
+
+leftScroll.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (mid > start) {
+      slides[mid].classList.add("slides");
+
+      mid = mid - 1;
+
+      slides[mid].classList.remove("slides");
+      console.log(mid);
+    } else if (mid === start) {
+      slides[mid].classList.add("slides");
+    //   slides[mid].classList.add('slider');
+
+      mid = end;
+      slides[mid].classList.remove("slides");
+    } else {
+      return;
+    }
+  });
 });
 
 console.log(slides);
 
-rightScroll.addEventListener( 'click', ()=>{
-    alert();
+rightScroll.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(mid);
+    if (mid < end) {
+    //   await (slides[mid].classList.add('slider'));
+
+      slides[mid].classList.add("slides");
+
+      mid = mid + 1;
+
+      slides[mid].classList.remove("slides");
+    //   await (slides[mid].classList.add('slider'));
+
+      console.log(mid);
+    } else if (mid === end) {
+      slides[mid].classList.add("slides");
+    //   slides[mid].classList.add('slider');
+
+      mid = start;
+    //   slides[mid].classList.add('slider');
+
+      slides[mid].classList.remove("slides");
+    } else {
+      return;
+    }
+  });
 });
-
-
-
